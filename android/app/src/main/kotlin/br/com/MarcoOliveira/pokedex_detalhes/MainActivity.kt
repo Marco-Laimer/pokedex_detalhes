de32f94f4c6e9c6083a3906ff821b3cb78f3ad0c
Marco Oliveira <makittu.laimer@gmail.com>
2021-05-30 19:25:48 -0300

    Importação da lib

---
+++ android/app/src/main/kotlin/br/com/MarcoOliveira/pokedex_detalhes/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.MarcoOliveira.pokedex_detalhes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_detalhes/screens/home/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Pokedex Detalhes",
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
